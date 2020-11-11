dfa5153496c0a00d918aac0062ea6a21ef464eec
Leo von Ahn <leotoyra@gmail.com>
2020-11-11 18:09:43 +0100

    Add entity class and direction enum

---
+++ Assets/Scripts/Entity.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class Entity : MonoBehaviour
+{
+    /// <summary>
+    /// Default: Return. The entity does nothing.
+    /// Otherwise: Moving entities attempt to Move(). Spikes toggle. Player waits for input.
+    /// </summary>
+    public void Action()
+    {
+        return;
+    }
+
+    /// <summary>
+    /// Ask stageManager what is in the adjacent space in the arguments direction.
+    /// Move there is possible, otherwise interact with it.
+    /// </summary>
+    /// <param name="aDirection"></param>
+    private void Move(Direction aDirection)
+    {
+
+    }
+
+    /// <summary>
+    /// When "moved" into, by the player usually. This is where a box is pushed for example.
+    /// </summary>
+    /// <param name="aDirection"></param>
+    public void Interact(Direction aDirection)
+    {
+
+    }
+}
