b3c7eee79211e90be38e16071aa5e437eb8d34eb
TGA-RobinKrokfors <71003044+TGA-RobinKrokfors@users.noreply.github.com>
2020-11-12 17:42:21 +0100

    More work on StageManager / turn machine

--- Assets/Scripts/Managers/StageManager.cs
+++ Assets/Scripts/Managers/StageManager.cs
@@ -1,29 +1,15 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
 public class StageManager : MonoBehaviour
 {
     // TODO: Remove
-    public class TestPlayer: TestEntityBase
-    {
-
-    }
-
-    // TODO: Remove
-    public class TestEntityBase : MonoBehaviour
-    {
-        public Vector2Int myPosition;
-
-        public void DoTurn() { }
-    }
-
-    // TODO: Remove
     public class TestTileBase : MonoBehaviour
     {
-        public Vector2Int myPosition;
-
-        public void OnEnter(TestEntityBase anEntity) { }
-        public void OnExit(TestEntityBase anEntity) { }
-        public bool CanEnter(TestEntityBase anEntity) => true;
+        public void OnEnter(Entity anEntity) { }
+        public void OnExit(Entity anEntity) { }
+        public bool CanEnter(Entity anEntity) => true;
     }
 
     public static StageManager ourInstance;
@@ -32,23 +18,25 @@ public class StageManager : MonoBehaviour
 
     [SerializeField]
     [Tooltip("The width of the grid where tiles & entities can be placed!")]
-    private int myGridWidth = 15;
+    private int myGridWidth = 10;
 
     [SerializeField]
     [Tooltip("The height of the grid where tiles & entities can be placed!")]
-    private int myGridHeight = 15;
+    private int myGridHeight = 10;
 
     [SerializeField]
     [Tooltip("The size of one tile in the grid")]
     private float myTileSize = 1.0f;
 
     // TODO: Change to TileBase
-    private TestTileBase[,] myTiles;
+    private TestTileBase[,] myTileGrid;
 
-    // TODO: Change to EntityBase
-    private TestEntityBase[,] myEntities;
+    private Entity[,] myEntityGrid;
 
-    private TestPlayer myPlayer;
+    // Use HashSet for fast iterate, remove and insert (assuming order is irrelevant)
+    private HashSet<Entity> myEntities = new HashSet<Entity>();
+
+    private Player myPlayer;
 
     public Vector3 GetTileCenterWorldPosition(Vector2Int aPosition)
     {
@@ -60,31 +48,30 @@ public class StageManager : MonoBehaviour
         return new Vector2Int(Mathf.FloorToInt(aPosition.x / myTileSize), Mathf.FloorToInt(aPosition.z / myTileSize));
     }
 
-    public void MoveEntity(TestEntityBase anEntity, Vector2Int aNewPosition)
+    public void MoveEntity(Entity anEntity, Vector2Int aNewPosition)
     {
-        Debug.Assert(CanEntityMoveToPosition(anEntity, aNewPosition), "Entity tried invalid move!");
-        Debug.Assert(myEntities[anEntity.myPosition.x, anEntity.myPosition.y] == anEntity, "Entity position in grid and grid manager not in sync!");
+        Vector2Int gridPosition = GetTilePositionFromWorld(anEntity.transform.position);
 
-        TestTileBase oldTile = myTiles[anEntity.myPosition.x, anEntity.myPosition.y];
+        Debug.Assert(CanEntityMoveToPosition(anEntity, aNewPosition), "Entity tried invalid move!");
+        Debug.Assert(myEntityGrid[gridPosition.x, gridPosition.y] == anEntity, "Entity position in grid and grid manager not in sync!");
+
+        TestTileBase oldTile = myTileGrid[gridPosition.x, gridPosition.y];
         if (oldTile != null)
         {
             oldTile.OnExit(anEntity);
         }
 
-        myEntities[anEntity.myPosition.x, anEntity.myPosition.y] = null;
-        myEntities[aNewPosition.x, aNewPosition.y] = anEntity;
+        myEntityGrid[gridPosition.x, gridPosition.y] = null;
+        myEntityGrid[aNewPosition.x, aNewPosition.y] = anEntity;
 
-        // Keep entity position in sync with grid
-        anEntity.myPosition = aNewPosition;
-
-        TestTileBase newTile = myTiles[aNewPosition.x, aNewPosition.y];
+        TestTileBase newTile = myTileGrid[aNewPosition.x, aNewPosition.y];
         if (newTile != null)
         {
             newTile.OnEnter(anEntity);
         }
     }
 
-    public bool CanEntityMoveToPosition(TestEntityBase anEntity, Vector2Int aNewPosition)
+    public bool CanEntityMoveToPosition(Entity anEntity, Vector2Int aNewPosition)
     {
         EnsurePositionInGrid(aNewPosition);
 
@@ -100,45 +87,110 @@ public class StageManager : MonoBehaviour
             return false;
         }
 
-        TestEntityBase entity = GetEntity(aNewPosition);
+        Entity entity = GetEntity(aNewPosition);
 
         // For now we allow moving to tile if there is no entity present!
         return entity == null;
     }
 
     // TODO: Change TileBase when available!
-    public void RegisterTile(TestTileBase aTile, Vector2Int aPosition)
+    public void RegisterTile(TestTileBase aTile)
     {
-        EnsureEmptyTile(aPosition);
+        Vector2Int gridPosition = GetTilePositionFromWorld(aTile.transform.position);
 
-        myTiles[aPosition.x, aPosition.y] = aTile;
+        EnsureEmptyTile(gridPosition);
+
+        myTileGrid[gridPosition.x, gridPosition.y] = aTile;
     }
 
-    // TODO: Change EntityBase when available!
-    public void RegisterEntity(TestEntityBase anEntity, Vector2Int aPosition)
+    public void RegisterEntity(Entity anEntity)
     {
-        EnsureNoEntity(aPosition);
+        Vector2Int gridPosition = GetTilePositionFromWorld(anEntity.transform.position);
 
-        myEntities[aPosition.x, aPosition.y] = anEntity;
+        EnsureNoEntity(gridPosition);
 
-        if (anEntity is TestPlayer)
+        myEntityGrid[gridPosition.x, gridPosition.y] = anEntity;
+        myEntities.Add(anEntity);
+
+        // TODO: When Player derives from Entity
+        /*
+        if (anEntity is Player)
         {
-            myPlayer = anEntity as TestPlayer;
-        }
+            myPlayer = anEntity as Player;
+        }*/
     }
 
     public TestTileBase GetTile(Vector2Int aPosition)
     {
         EnsurePositionInGrid(aPosition);
 
-        return myTiles[aPosition.x, aPosition.y];
+        return myTileGrid[aPosition.x, aPosition.y];
     }
 
-    public TestEntityBase GetEntity(Vector2Int aPosition)
+    public Entity GetEntity(Vector2Int aPosition)
     {
         EnsurePositionInGrid(aPosition);
 
-        return myEntities[aPosition.x, aPosition.y];
+        return myEntityGrid[aPosition.x, aPosition.y];
+    }
+
+    private IEnumerator TurnMachine()
+    {
+        // Wait 1 frame to allow all tiles & entities to be registered
+        yield return null;
+
+        // Allocate most (all if no reallocations are needed) memory needed by the turn machine at once before we start the main loop
+        List<TurnEvent> incompleteNonPlayerTurnEvents = new List<TurnEvent>(myEntities.Count);
+        TurnCache turnCache = new TurnCache(myEntities.Count);
+
+        do
+        {
+            incompleteNonPlayerTurnEvents.Clear();
+
+            // TODO: Uncomment when player derives from Entity
+            /*TurnEvent playerTurnEvent = turnCache.Next();
+            myPlayer.Action(playerTurnEvent);
+
+            if (!playerTurnEvent.myIsTurnDone)
+            {
+                yield return playerTurnEvent.myTurnYield;
+            }
+
+            turnCache.Recycle(playerTurnEvent);*/
+
+            foreach (Entity entity in myEntities)
+            {
+                if (entity == myPlayer)
+                    continue; // Skip the player (handled above)
+
+                TurnEvent turnEvent = turnCache.Next();
+                entity.Action(turnEvent);
+
+                // Optimization
+                if (!turnEvent.myIsTurnDone)
+                {
+                    incompleteNonPlayerTurnEvents.Add(turnEvent);
+                }
+                else
+                {
+                    turnCache.Recycle(turnEvent);
+                }
+            }
+
+            foreach (TurnEvent turnEvent in incompleteNonPlayerTurnEvents)
+            {
+                if (!turnEvent.myIsTurnDone)
+                {
+                    yield return turnEvent.myTurnYield;
+                }
+
+                turnCache.Recycle(turnEvent);
+            }
+
+            yield return null;
+
+            // TODO: Check game should continue (player alive check possibly)
+        } while (true);
     }
 
     [System.Diagnostics.Conditional("UNITY_EDITOR")]
@@ -164,8 +216,13 @@ public class StageManager : MonoBehaviour
     {
         Debug.Assert(myGridWidth > 0 && myGridHeight > 0, "Grid dimensions invalid!");
 
-        myTiles = new TestTileBase[myGridWidth, myGridHeight];
-        myEntities = new TestEntityBase[myGridWidth, myGridHeight];
+        myTileGrid = new TestTileBase[myGridWidth, myGridHeight];
+        myEntityGrid = new Entity[myGridWidth, myGridHeight];
+    }
+
+    private void Start()
+    {
+        StartCoroutine(TurnMachine());
     }
 
     private void Awake()
