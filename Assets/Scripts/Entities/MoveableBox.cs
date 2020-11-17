55306e8eb98ed1c461dbcd15eab1aec67d69b4f9
OskarEmanuelssonEdu <oskar.emanuelsson@studerande.thegameassembly.se>
2020-11-17 16:45:38 +0100

    Push Function
    A function which pushes a box

---
+++ Assets/Scripts/Entities/MoveableBox.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveableBox : Entity
+{
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void Push(Direction aDirection)
+    {
+        Move(aDirection);
+    }
+}
