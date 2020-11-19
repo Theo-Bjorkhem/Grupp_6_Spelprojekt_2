168692ac4a8623cd48677d6b441ac72135a36ce4
Tomas Granlund <tomas.granlund@studerande.thegameassembly.se>
2020-11-19 12:02:23 +0100

    Fixed the WinTile after testing it

--- Assets/Scripts/Tiles/WinTile.cs
+++ Assets/Scripts/Tiles/WinTile.cs
@@ -2,11 +2,26 @@
 
 public class WinTile : Tile
 {
+    protected override void Start()
+    {
+        base.Start();
+
+        StageManager.ourInstance.RegisterTileForTurnEvents(this);
+    }
+
     public override void OnEnter(Entity steppedOnMe)
     {
         if (steppedOnMe as Player != null)
         {
-            GameManager.ourInstance.TransitionToNextStage();
+            if (GameManager.ourInstance == null)
+            {
+                Debug.LogWarning("Cannot transition to next stage because GameManager is null. Did you start the game from the main menu?", this);
+            }
+            else
+            {
+                StageManager.ourInstance.OnPlayerWon();
+                GameManager.ourInstance.TransitionToNextStage();
+            }
         }
         base.OnEnter(steppedOnMe);
     }
