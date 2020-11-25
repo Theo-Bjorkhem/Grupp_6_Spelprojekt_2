1ce112281bfa61f659b5da1aabf4cd71c9d90704
T.B <71003024+Theo-Bjorkhem@users.noreply.github.com>
2020-11-25 17:36:09 +0100

    Paus Meny Finns Typ

---
+++ Assets/Scripts/UI/Pause_script.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pause_script : MonoBehaviour
+{
+    public void PauseGame()
+    {
+        gameObject.SetActive(true);
+    }
+    public void UnPasueGame()
+    {
+        gameObject.SetActive(false);
+    }
+
+    void Update()
+    {
+        
+    }
+}
