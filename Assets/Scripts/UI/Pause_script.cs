1f06c47854dad4befba5b59770f668c55e851d12
T.B <71003024+Theo-Bjorkhem@users.noreply.github.com>
2020-11-27 17:10:15 +0100

    Paus Scriptet Fungerar

--- Assets/Scripts/UI/Pause_script.cs
+++ Assets/Scripts/UI/Pause_script.cs
@@ -1,20 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Pause_script : MonoBehaviour
 {
     public void PauseGame()
     {
-        gameObject.SetActive(true);
+        Time.timeScale = 0;
+        SceneManager.LoadScene("PausMenu1_scene", LoadSceneMode.Additive);
     }
     public void UnPasueGame()
     {
-        gameObject.SetActive(false);
+        Destroy(SceneManager.GetSceneByName("PausMenu1_scene").GetRootGameObjects()[0]);
+        SceneManager.UnloadSceneAsync("PausMenu1_scene");
+        Time.timeScale = 1;
     }
-
-    void Update()
+    public void LoadMainMenu()
     {
-        
+
     }
 }
