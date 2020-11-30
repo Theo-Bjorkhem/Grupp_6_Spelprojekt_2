3e7bccaf1b0ae02e59b2db5e4a44a284e609e137
TGA-RobinKrokfors <71003044+TGA-RobinKrokfors@users.noreply.github.com>
2020-11-30 17:41:50 +0100

    EffectUI & simple tweening implementation - Fade out & in when loading scene from GameManager

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -79,7 +79,7 @@ public class GameManager : MonoBehaviour
 
     private IEnumerator TransitionToScene(string aSceneName)
     {
-        // TODO: Fade out?
+        yield return EffectUI.ourInstance.FadeOut(0.8f);
 
         // TODO: Start loading animation?
 
@@ -87,7 +87,7 @@ public class GameManager : MonoBehaviour
 
         // TODO: Stop loading animation?
 
-        // TODO: Fade in?
+        yield return EffectUI.ourInstance.FadeIn(0.8f);
     }
 
     private void Awake()
@@ -101,5 +101,7 @@ public class GameManager : MonoBehaviour
         DontDestroyOnLoad(gameObject);
 
         ourInstance = this;
+
+        SceneManager.LoadScene("effectUi_scene", LoadSceneMode.Additive);
     }
 }
