9f31b9c571c50a671c6b2afdb969ec8bcfbd5f2d
Leo von Ahn <leotoyra@gmail.com>
2020-11-30 16:04:17 +0100

    play sounds from pause script

--- Assets/Scripts/UI/Pause_script.cs
+++ Assets/Scripts/UI/Pause_script.cs
@@ -10,17 +10,20 @@ public class Pause_script : MonoBehaviour
         if (!SceneManager.GetSceneByName("PausMenu1_scene").isLoaded)
         {
             Time.timeScale = 0;
+            AudioManager.ourInstance.PlaySound("Pause");
             SceneManager.LoadScene("PausMenu1_scene", LoadSceneMode.Additive);
         }
     }
     public void UnPasueGame()
     {
         Destroy(SceneManager.GetSceneByName("PausMenu1_scene").GetRootGameObjects()[0]);
+        AudioManager.ourInstance.PlaySound("Unpause");
         SceneManager.UnloadSceneAsync("PausMenu1_scene");
         Time.timeScale = 1;
     }
     public void LoadMainMenu()
     {
+        AudioManager.ourInstance.PlaySound("MenuNegative");
         SceneManager.LoadScene("mainMenu_scene");
         Time.timeScale = 1;
     }
