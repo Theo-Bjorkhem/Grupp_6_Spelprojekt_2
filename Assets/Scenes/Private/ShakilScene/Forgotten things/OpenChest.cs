e42d7923d2db2ee347763a3ec76a27a7ed8b1cd3
Shakil-SP20 <shakil.islam@studerande.thegameassembly.se>
2020-12-02 16:59:14 +0100

    Placeholder Animated chest finally complete
    Changed a bit on LockedBox.cs to accomadate Animated chest.

---
+++ Assets/Scenes/Private/ShakilScene/Forgotten things/OpenChest.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(KeyAnimator))]
+public class OpenChest : Entity
+{
+    //Animator animator;
+
+    //// Start is called before the first frame update
+    //void Start()
+    //{
+    //    animator = GetComponent<Animator>();
+    //}
+
+    //private void OnTriggerEnter(Collider collider)
+    //{
+    //    if(collider.tag == "Player")
+    //    {
+    //        animator.SetTrigger("OpenChest");
+    //    }
+    //}
+
+    // animator.SetTrigger("OpenChest");
+
+    private Animator myAnimator;
+
+    public override InteractResult Interact(Entity anEntity, Direction aDirection)
+    {
+        base.Interact(anEntity, aDirection);
+        print(StageManager.ourInstance.myHasKey);
+
+        if (StageManager.ourInstance.myHasKey)
+        {
+            if (AudioManager.ourInstance != null)
+            {
+                AudioManager.ourInstance.PlaySound("UnlockChest");
+            }
+
+            StageManager.ourInstance.UnregisterEntity(this);
+            this.gameObject.SetActive(false);
+            return InteractResult.Unlocked;
+        }
+        return InteractResult.UnlockFailed;
+    }
+}
+
