be8fd2ec3eefc47a48ccaf8d0721948667c2f1bf
Tomas Granlund <tomas.granlund@studerande.thegameassembly.se>
2020-12-16 11:15:34 +0100

    More parameters for the mist

--- Assets/Scripts/ParticleSystemParent.cs
+++ Assets/Scripts/ParticleSystemParent.cs
@@ -7,6 +7,8 @@ public class ParticleSystemParent : MonoBehaviour
     [SerializeField] float mySpawnRate = 2.0f;
     [SerializeField] float myLifetime = 30.0f;
     [SerializeField] float mySpeed = 0.1f;
+    [SerializeField] int myMaxParticles = 50;
+    [SerializeField] float myDuration = 10.0f;
 
     public void ApplyChanges()
     {
@@ -20,6 +22,8 @@ public class ParticleSystemParent : MonoBehaviour
             emission.rateOverTimeMultiplier = mySpawnRate;
             main.startLifetimeMultiplier = myLifetime;
             main.startSpeedMultiplier = mySpeed;
+            main.maxParticles = myMaxParticles;
+            main.duration = myDuration;
         }
     }
 }
