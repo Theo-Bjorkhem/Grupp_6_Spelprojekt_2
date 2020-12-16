cad1c3bc13005f6dc1747a9416cf852a83b026e8
Tomas Granlund <tomas.granlund@studerande.thegameassembly.se>
2020-12-16 17:18:27 +0100

    Mist optimizations

--- Assets/Scripts/ParticleSystemParent.cs
+++ Assets/Scripts/ParticleSystemParent.cs
@@ -4,26 +4,36 @@
 public class ParticleSystemParent : MonoBehaviour
 {
     [SerializeField] Gradient myColorOverLifetime;
-    [SerializeField] float mySpawnRate = 2.0f;
+    [SerializeField] float mySpawnRate = 0.5f;
     [SerializeField] float myLifetime = 30.0f;
     [SerializeField] float mySpeed = 0.1f;
     [SerializeField] int myMaxParticles = 50;
-    [SerializeField] float myDuration = 10.0f;
+    [SerializeField] float myDuration = 30.0f;
 
     public void ApplyChanges()
     {
+        ParticleSystem onMe = GetComponent<ParticleSystem>();
+        if (onMe != null)
+        {
+            ApplyTo(onMe);
+        }
         foreach (var system in GetComponentsInChildren<ParticleSystem>())
         {
-            var colorOverLifetime = system.colorOverLifetime;
-            var emission = system.emission;
-            var main = system.main;
-
-            colorOverLifetime.color = myColorOverLifetime;
-            emission.rateOverTimeMultiplier = mySpawnRate;
-            main.startLifetimeMultiplier = myLifetime;
-            main.startSpeedMultiplier = mySpeed;
-            main.maxParticles = myMaxParticles;
-            main.duration = myDuration;
+            ApplyTo(system);
         }
+    }
+
+    void ApplyTo(ParticleSystem system)
+    {
+        var colorOverLifetime = system.colorOverLifetime;
+        var emission = system.emission;
+        var main = system.main;
+
+        colorOverLifetime.color = myColorOverLifetime;
+        emission.rateOverTimeMultiplier = mySpawnRate;
+        main.startLifetimeMultiplier = myLifetime;
+        main.startSpeedMultiplier = mySpeed;
+        main.maxParticles = myMaxParticles;
+        main.duration = myDuration;
     }
 }
